5f28f5ff661ceee47a98326f9f353e6fae5f0b06
Eugene Bednik <eugene.bednik@leafgroup.com>
2020-12-31 04:49:49 -0800

    blank target for adding the bot:

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
                         <h3>Installation Instructions</h3>
                         <p>Please follow these instructions to add the bot and activate the dashboard for your server:</p>
                         <ul>
-                            <li>Add the Discord bot to your discord server by clicking this link: <a href="https://discord.com/oauth2/authorize?client_id=770374612084195328&scope=bot&permissions=537357376">Add the bot to your Discord server</a>.</li>
+                            <li>Add the Discord bot to your discord server by clicking this link: <a href="https://discord.com/oauth2/authorize?client_id=770374612084195328&scope=bot&permissions=537357376" target="_blank">add the bot to your Discord server</a>.</li>
                             <li>On your Discord server, go to the channel where you would like the Protocol Officer bot to operate and run the following command: <pre>!pobot setup</pre></li>
                             <li>The bot will create your server within the Dashboard app and provide you with a server link that looks like this: <pre>https://your-server.protocolofficer.net</pre></li>
                             <li>Email <code>Daenelys</code> in game with the link the bot provided you with so that I can activate your Dashboard account.</li>
