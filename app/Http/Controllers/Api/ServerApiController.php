7b78ed9469adc7a28f5c1d11bca57faf1db87977
Eugene Bednik <eugene.b@akesosys.com>
2021-09-29 23:36:49 -0700

    Do not require activation

--- app/Http/Controllers/Api/ServerApiController.php
+++ app/Http/Controllers/Api/ServerApiController.php
@@ -27,7 +27,7 @@ class ServerApiController extends Controller
             $server = Server::create([
                 'snowflake' => $snowflake,
                 'name' => Str::slug($request->input('name')),
-                'is_active' => false,
+                'is_active' => true,
                 'webhook_id' => $request->input('webhook_id'),
                 'webhook_token' => $request->input('webhook_token'),
             ]);
